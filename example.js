a63479c7bf60d96031f609228bffca8865dca2d6
Sayem Chowdhury <sayem314@gmail.com>
2020-01-14 12:30:16 +0600

    Update example

--- example.js
+++ example.js
@@ -1,17 +1,24 @@
 const torrentIndexer = require("./");
 
 (async function() {
+  // search tv series
   const series = await torrentIndexer.search("rick and morty s04e04", "series");
   console.log("Series", series[0], series.length + " items");
 
+  // link contains magnet, infohash or torrent
   if (!series[0].link) {
-    const torrentInfo = torrentIndexer.info(series[0].site);
+    const torrentInfo = torrentIndexer.torrent(series[0].site);
+    // returns magnet or infohash
   }
 
+  // search movies
   const movies = await torrentIndexer.search(
     "x-men: dark phoenix 2019",
     "movie"
   );
-
   console.log("Movies", movies[0], movies.length + " items");
+
+  // search music or other thing
+  const music = await torrentIndexer.search("lana del rey");
+  console.log("Music", music[0], music.length + " items");
 })();
