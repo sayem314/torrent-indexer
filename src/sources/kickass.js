bcb49a17533eabb720c590715c21c913d5f9d3f7
Sayem Chowdhury <sayem314@gmail.com>
2020-05-01 19:15:58 +0600

    Support kickass #5

---
+++ src/sources/kickass.js
@@ -0,0 +1,55 @@
+const TorrentSource = require("../lib/torrentSource");
+const axios = require("../lib/request");
+const unhumanizeSize = require("../lib/unhumanizeSize");
+const { parse } = require("node-html-parser");
+
+class Kickass extends TorrentSource {
+  constructor(options) {
+    super(options.name);
+    this.url = options.url;
+  }
+
+  async search(query, type, page = 1, category) {
+    try {
+      const search_url = `${
+        this.url
+      }/katsearch/page/${page}/${encodeURIComponent(query)}`;
+      const torrent_content = [];
+
+      const { data } = await axios.get(search_url);
+      const root = parse(data).querySelectorAll("tbody tr");
+
+      for (const element of root) {
+        const a = element.querySelectorAll("td a");
+        const info = element.querySelectorAll("td");
+        const size = info[1].text;
+
+        // Sometimes link on position 6 contains ads link
+        let link = a[6].attributes.href;
+        if (!link.startsWith("magnet:?xt=urn:btih:")) {
+          link = a[5].attributes.href;
+        }
+
+        torrent_content.push({
+          fileName: a[0].text.trim(),
+          category: a[2].text,
+          seeders: Number(info[4].text),
+          leechers: Number(info[5].text),
+          uploaded: info[3].text,
+          uploader: a[1].text.trim(),
+          size,
+          length: unhumanizeSize(size),
+          link
+        });
+      }
+
+      return this.reconstitute(torrent_content, query, type);
+    } catch (err) {
+      console.log("\u2717 There was a problem loading " + this.sourceName);
+      console.error(err.message);
+      return [];
+    }
+  }
+}
+
+module.exports = Kickass;
---
+++ src/sources/kickass.test.js
@@ -0,0 +1,39 @@
+const test = require("ava");
+const Kickass = require("./kickass");
+const { sources } = require("../config");
+const { expect } = require("chai");
+
+const kickass = new Kickass(sources.kickass);
+
+test("should work properly", async () => {
+  const results = await kickass.search("rick and morty s04e01");
+  expect(results).to.be.an("array");
+  expect(results.length).to.be.at.least(5);
+
+  for (const item of results) {
+    expect(item.title.toLowerCase()).to.be.equal("rick and morty");
+    expect(item.fileName).to.be.an("string");
+    expect(item.fileName.length).to.be.at.least(10);
+
+    expect(item.seeders).to.be.an("number");
+    expect(item.seeders).to.be.least(0);
+
+    expect(item.leechers).to.be.an("number");
+    expect(item.leechers).to.be.least(0);
+
+    expect(item.uploaded).to.be.an("string");
+    expect(item.uploaded.length).to.be.at.least(10);
+
+    expect(item.uploader).to.be.an("string");
+    expect(item.uploader.length).to.be.at.least(3);
+
+    expect(item.size).to.be.an("string");
+    expect(item.size).to.include("B");
+
+    expect(item.length).to.be.an("number");
+    expect(item.length).to.be.least(1024);
+
+    expect(item.link).to.be.an("string");
+    expect(item.link).to.include("magnet:?xt=urn:btih:");
+  }
+});
